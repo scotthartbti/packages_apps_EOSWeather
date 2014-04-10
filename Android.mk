a6459758862d13b547a65107e3eee96cfd48d1ad
Randall Rushing <randall.rushing@gmail.com>
2014-04-10 03:28:14 -0700

    Update module name

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
-LOCAL_PACKAGE_NAME := CFXWeather
+LOCAL_PACKAGE_NAME := EOSWeather
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
